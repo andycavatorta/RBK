f1aa888c38bf68dca5b61039d7f50a38cf4c3dfe
andycavatorta <andycavatorta@gmail.com>
2016-06-19 15:52:34 -0400

    sending parallel twice quickly to test

--- common/x24bitParallelPort.py
+++ common/x24bitParallelPort.py
@@ -12,7 +12,7 @@ if PI_NATIVE:
 else:
     import RPi_stub.GPIO as GPIO
 
-INTRA_WORD_TIMING = 0.1 # SECONDS
+INTRA_WORD_TIMING = 0.01 # SECONDS
 #TX_PINS = [2,3,14,4,15,18,17,27,23,24,10,9,25,11,8,7,5,6,12,13,16,19,20,26] # pin 22 is wired but not used
 #TX_PINS = [14,4,15,18,17,27,23,24,10,9,25,11,8,7,5,6,12,13,16,19,20,26,21] # pin 22 is wired but not used
 TX_PINS = [26,20,19,16,13,6,12,5,7,11,8,9,25,10,24,22,23,27,17,18,15,4,14] # pin 22 is wired but not used
@@ -29,11 +29,11 @@ def send(word_bin_a): # word_ba must be 23 bit-characters long
 
     
     GPIO.output(DATA_READY_PIN  ,0)
-    word_bin_a += [0] * (len(TX_PINS) - len(word_bin_a)) # pad word_bin_a with zeroes
-    for i in range(len(TX_PINS)):
+    #word_bin_a += [0] * (len(TX_PINS) - len(word_bin_a)) # pad word_bin_a with zeroes
+    #for i in range(len(TX_PINS)):
         #print "x24ParallalPort.send ", i, TX_PINS[i], int(word_bin_a[i])
-        GPIO.output(TX_PINS[i],int(word_bin_a[i]))
-    time.sleep(INTRA_WORD_TIMING) # this is a cheap-ass solution.  But it's simple and efficient.  Could replace with threads and queues in the future.
+    #    GPIO.output(TX_PINS[i],int(word_bin_a[i]))
+    #time.sleep(INTRA_WORD_TIMING) # this is a cheap-ass solution.  But it's simple and efficient.  Could replace with threads and queues in the future.
     GPIO.output(DATA_READY_PIN,1)
     
     GPIO.output(DATA_READY_PIN  ,0)
