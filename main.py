df4711255dfdf9f8afe40cac2efbadf6444f0266
andycavatorta <andycavatorta@gmail.com>
2016-08-08 16:07:14 -0400

    moving/cleaning main.py, adding mapper.  hasty commit to sync with Joao

--- main.py
+++ main.py
@@ -1,59 +1,124 @@
-""" IMPORTS AND GLOBALS """
+#############################################
+##### MODULES, EVENIRONMENT AND GLOBALS #####
+#############################################
 
 import json
 import os
-import socket
+import struct
+import time
+import threading
 import sys
+import socket
+#import errorlog as elog
 
-BASE_PATH = "%s/%s" % os.path.split(os.path.dirname(os.path.realpath(__file__)))
-
+# constants
+#PI_NATIVE = os.uname()[4].startswith("arm") # TRUE if running on RPi
+#BASE_PATH = "/home/pi/nervebox_2/" if PI_NATIVE else "/home/stella/Dropbox/projects/current/nervebox_2/" 
+HOSTNAME = socket.gethostname()
+#HOSTNAME = "R8MKII"
+BASE_PATH = "%s/%s" % (os.path.split(os.path.dirname(os.path.realpath(__file__)))[0], os.path.split(os.path.dirname(os.path.realpath(__file__)))[1])
+CLIENT_PATH = "%s/client/" % (BASE_PATH )
+DEVICES_PATH = "%s/client/devices/" % (BASE_PATH )
 COMMON_PATH = "%s/common/" % (BASE_PATH )
-DATA_PATH = "%s/data/" % (BASE_PATH )
-LOG_PATH = "%s/logs/" % (BASE_PATH )
+HOST_SPECIFIC_PATH = "%s/client/devices/%s/" % (BASE_PATH, HOSTNAME)
+SERVER_PATH = "%s/server/" % (BASE_PATH )
+STORE_PATH = "%s/store/" % SERVER_PATH
+clientnames = ("blueberrypie","blackberrypie")
+ROLE = sys.argv[1]
+possible_responses = ["client", "server", "dashboard"]
+while ROLE not in possible_responses:
+    ROLE = raw_input('Please type dashboard, client or server: ')
+if ROLE == "server":
+    HOSTNAME = "**SERVER**"
+if ROLE == "dashboard":
+    HOSTNAME = "**DASHBOARD**"
+print 'role: ', ROLE
+print 'paths loaded'
+
+# local paths
 sys.path.append(BASE_PATH)
 sys.path.append(COMMON_PATH)
+sys.path.append(CLIENT_PATH)
+sys.path.append(SERVER_PATH)
+sys.path.append(HOST_SPECIFIC_PATH)
 
-import githubSync
-import packageManager
+print "path append ok"
 
-SETTINGS = {} # overwritten below
+device = None
 
-""" START GLOBAL SYSTEMS """
+print os.path.split(os.path.dirname(os.path.realpath(__file__)))
 
-def errorlogger(err_t):
-    if err_t[0] > 0:
-        print "errorlogger:", err_t[2], err_t[1]
+try:
+    # import local modules
+    import dps
+    import nerveOSC
+    if ROLE == "client":
+        device = __import__('device')
+        device.init()
+    else:
+        import parseOsc
+        import midiToOsc
+        import midiDeviceManager
+        import midiserver
 
-packageManager.update(
-    BASE_PATH,
-    DATA_PATH,
-    LOG_PATH,
-    COMMON_PATH,
-    errorlogger
-)
+    print "import local modules ok"
 
-""" FETCH NEW PYTHON CODE """
+    # load config
+    with open(COMMON_PATH + 'settings.json', 'r') as f:
+        SETTINGS = json.load(f)
 
-#stat_t = githubSync.main(BASE_PATH)
-#print repr(stat_t)
-#errorlogger(stat_t)
-
-with open(COMMON_PATH + 'settings.json', 'r') as f:
-    SETTINGS = json.load(f, encoding="UTF-8")
-
-
-""" LOAD HOST-SPECIFIC CODE """
-"""
-HOSTNAME = socket.gethostname()
-if unicode(HOSTNAME) in SETTINGS["servers"]:
-	print "load server"
-else:
-	CLIENT_PATH = "%s/client/" % (BASE_PATH )
-	sys.path.append(CLIENT_PATH)
-	import client_main
-"""
-""" NETWORK DISCOVERY AND SELF-ASSEMBLY """
+    #with open(COMMON_PATH + 'mappings.json', 'r') as f:
+    #    MAPPINGS = json.load(f)
+    #
+    #print "load config ok"
+    #
+    #MAPPING = MAPPINGS["MAPPINGS"]["default"] # todo: the mapping name will have to be dynamically updated
 
 
 
+    ######################
+    ##### NETWORKING #####
+    ######################
 
+    if ROLE == "client":
+        subscribernames = ["nervebox"]
+        import mapping  # host-specific mapping
+        import midiOutput
+        midi_output = midiOutput.Midi_Output()
+        print mapping
+        #def osc_handler(msg):
+
+
+            
+
+    else:
+        subscribernames = filter(lambda x: os.path.isdir(os.path.join(DEVICES_PATH, x)), os.listdir(DEVICES_PATH))
+        subscribernames.append("nervebox2")
+        subscribernames.append(HOSTNAME)
+
+    print "subscribernames ok"
+    print "initializing network..."
+
+    if ROLE == 'server': 
+        import vimina
+        def osc_handler(msg):
+            pass
+
+
+    dps.init_networking(
+        subscribernames,
+        HOSTNAME,
+        ROLE,
+        SETTINGS["pubsub_pubPort"],
+        SETTINGS["pubsub_pubPort2"],
+        SETTINGS["discovery_multicastGroup"],
+        SETTINGS["discovery_multicastPort"],
+        SETTINGS["discovery_multicastPort2"],
+        SETTINGS["discovery_responsePort"],
+        SETTINGS["discovery_responsePort2"],
+        device
+    )
+
+except Exception as e:
+    print e
+    #elog.elog.logerror()
