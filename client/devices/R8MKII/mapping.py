fb3f1610e8222054de12fdf54c2ad03d3f3a54b4
João Costa <joaopedrocosta@me.com>
2016-08-08 13:45:27 -0400

    Mapping
    Added mapping for HR16, R8MKII and DR550MKII

--- client/devices/R8MKII/mapping.py
+++ client/devices/R8MKII/mapping.py
@@ -1,120 +1,330 @@
 Mapping = {
-    "/sound/tom_1/bang":(
-        ("MIDI", {channel:1, pitch:48 ,status:"note_on"})
-    ),
-    "/sound/tom_1/off":(
-        ("MIDI", {channel:1, pitch:48 ,status:"note_off"})
-    ),
-
-    "/sound/tom_2/bang":(
-        ("MIDI", {channel:1, pitch:48 ,status:"note_on"})
-    ),
-    "/sound/tom_2/off":(
-        ("MIDI", {channel:1, pitch:48 ,status:"note_off"})
-    ),
-
-    "/sound//bang":(
-        ("MIDI", {channel:1, pitch:45 ,status:"note_on"})
-    ),
-    "/sound//off":(
-        ("MIDI", {channel:1, pitch:45 ,status:"note_off"})
-    ),
-
-    "/sound/tom_3/bang":(
-        ("MIDI", {channel:1, pitch:41 ,status:"note_on"})
-    ),
-    "/sound/tom_3/off":(
-        ("MIDI", {channel:1, pitch:41 ,status:"note_off"})
-    ),
-
-    "/sound/tom_4/bang":(
-        ("MIDI", {channel:1, pitch:63 ,status:"note_on"})
-    ),
-    "/sound/tom_4/off":(
-        ("MIDI", {channel:1, pitch:63 ,status:"note_off"})
-    ),
-
-    "/sound/ride/bang":(
-        ("MIDI", {channel:1, pitch:51 ,status:"note_on"})
-    ),
-    "/sound/ride/off":(
-        ("MIDI", {channel:1, pitch:51 ,status:"note_off"})
-    ),
-
-    "/sound/crash/bang":(
-        ("MIDI", {channel:1, pitch:49 ,status:"note_on"})
-    ),
-    "/sound/crash/off":(
-        ("MIDI", {channel:1, pitch:49 ,status:"note_off"})
-    ),
-
-    "/sound/perc_1/bang":(
-        ("MIDI", {channel:1, pitch:65 ,status:"note_on"})
-    ),
-    "/sound/perc_1/off":(
-        ("MIDI", {channel:1, pitch:65 ,status:"note_off"})
-    ),
-
-    "/sound/perc_2/bang":(
-        ("MIDI", {channel:1, pitch:62 ,status:"note_on"})
-    ),
-    "/sound/perc_2/off":(
-        ("MIDI", {channel:1, pitch:62 ,status:"note_off"})
-    ),
-
-    "/sound/kick/bang":(
+    "/sound/bass_drum2/bang":(
         ("MIDI", {channel:1, pitch:35 ,status:"note_on"})
     ),
-    "/sound/kick/off":(
+    "/sound/bass_drum2/off":(
         ("MIDI", {channel:1, pitch:35 ,status:"note_off"})
     ),
 
-    "/sound/snare/bang":(
+    "/sound/bass_drum/bang":(
+        ("MIDI", {channel:1, pitch:36 ,status:"note_on"})
+    ),
+    "/sound/bass_drum/off":(
+        ("MIDI", {channel:1, pitch:36 ,status:"note_off"})
+    ),
+
+    "/sound/side_stick/bang":(
+        ("MIDI", {channel:1, pitch:37 ,status:"note_on"})
+    ),
+    "/sound/side_stick/off":(
+        ("MIDI", {channel:1, pitch:37 ,status:"note_off"})
+    ),
+
+    "/sound/snare_drum1/bang":(
         ("MIDI", {channel:1, pitch:38 ,status:"note_on"})
     ),
-    "/sound/snare/off":(
+    "/sound/snare_drum1/off":(
         ("MIDI", {channel:1, pitch:38 ,status:"note_off"})
     ),
 
-    "/sound/closed_hat/bang":(
-        ("MIDI", {channel:1, pitch:42 ,status:"note_on"})
-    ),
-    "/sound/closed_hat/off":(
-        ("MIDI", {channel:1, pitch:42 ,status:"note_off"})
-    ),
-
-    "/sound/mid_hat/bang":(
-        ("MIDI", {channel:1, pitch:44 ,status:"note_on"})
-    ),
-    "/sound/mid_hat/off":(
-        ("MIDI", {channel:1, pitch:44 ,status:"note_off"})
-    ),
-
-    "/sound/open_hat/bang":(
-        ("MIDI", {channel:1, pitch:46 ,status:"note_on"})
-    ),
-    "/sound/open_hat/off":(
-        ("MIDI", {channel:1, pitch:46 ,status:"note_off"})
-    ),
-
-    "/sound/claps/bang":(
+    "/sound/hand_clap/bang":(
         ("MIDI", {channel:1, pitch:39 ,status:"note_on"})
     ),
-    "/sound/clapsperc_3/off":(
+    "/sound/hand_clap/off":(
         ("MIDI", {channel:1, pitch:39 ,status:"note_off"})
     ),
 
-    "/sound/perc_3/bang":(
+    "/sound/snare_drum2/bang":(
+        ("MIDI", {channel:1, pitch:40 ,status:"note_on"})
+    ),
+    "/sound/snare_drum2/off":(
+        ("MIDI", {channel:1, pitch:40 ,status:"note_off"})
+    ),
+
+    "/sound/low_tom2/bang":(
+        ("MIDI", {channel:1, pitch:41 ,status:"note_on"})
+    ),
+    "/sound/low_tom2/off":(
+        ("MIDI", {channel:1, pitch:41 ,status:"note_off"})
+    ),
+
+    "/sound/closed_hihat/bang":(
+        ("MIDI", {channel:1, pitch:42 ,status:"note_on"})
+    ),
+    "/sound/closed_hihat/off":(
+        ("MIDI", {channel:1, pitch:42 ,status:"note_off"})
+    ),
+
+    "/sound/low_tom1/bang":(
+        ("MIDI", {channel:1, pitch:43 ,status:"note_on"})
+    ),
+    "/sound/low_tom1/off":(
+        ("MIDI", {channel:1, pitch:43 ,status:"note_off"})
+    ),
+
+    "/sound/pedal_hihat/bang":(
+        ("MIDI", {channel:1, pitch:44 ,status:"note_on"})
+    ),
+    "/sound/pedal_hihat/off":(
+        ("MIDI", {channel:1, pitch:44 ,status:"note_off"})
+    ),
+
+    "/sound/mid_tom2/bang":(
+        ("MIDI", {channel:1, pitch:45 ,status:"note_on"})
+    ),
+    "/sound/mid_tom2/off":(
+        ("MIDI", {channel:1, pitch:45 ,status:"note_off"})
+    ),
+
+    "/sound/open_hihat/bang":(
+        ("MIDI", {channel:1, pitch:46 ,status:"note_on"})
+    ),
+    "/sound/open_hihat/off":(
+        ("MIDI", {channel:1, pitch:46 ,status:"note_off"})
+    ),
+
+    "/sound/mid_tom1/bang":(
+        ("MIDI", {channel:1, pitch:47 ,status:"note_on"})
+    ),
+    "/sound/mid_tom1/off":(
+        ("MIDI", {channel:1, pitch:47 ,status:"note_off"})
+    ),
+
+    "/sound/high_tom2/bang":(
+        ("MIDI", {channel:1, pitch:48 ,status:"note_on"})
+    ),
+    "/sound/high_tom2/off":(
+        ("MIDI", {channel:1, pitch:48 ,status:"note_off"})
+    ),
+
+    "/sound/crash_cymbal/bang":(
+        ("MIDI", {channel:1, pitch:49 ,status:"note_on"})
+    ),
+    "/sound/crash_cymbal/off":(
+        ("MIDI", {channel:1, pitch:49 ,status:"note_off"})
+    ),
+
+    "/sound/high_tom1/bang":(
+        ("MIDI", {channel:1, pitch:50 ,status:"note_on"})
+    ),
+    "/sound/high_tom1/off":(
+        ("MIDI", {channel:1, pitch:50 ,status:"note_off"})
+    ),
+
+    "/sound/ride_cymbal1/bang":(
+        ("MIDI", {channel:1, pitch:51 ,status:"note_on"})
+    ),
+    "/sound/ride_cymbal1/off":(
+        ("MIDI", {channel:1, pitch:51 ,status:"note_off"})
+    ),
+
+    "/sound/chinese_cymbal/bang":(
+        ("MIDI", {channel:1, pitch:52 ,status:"note_on"})
+    ),
+    "/sound/chinese_cymbal/off":(
+        ("MIDI", {channel:1, pitch:52 ,status:"note_off"})
+    ),
+
+    "/sound/ride_bell/bang":(
+        ("MIDI", {channel:1, pitch:53 ,status:"note_on"})
+    ),
+    "/sound/ride_bell/off":(
+        ("MIDI", {channel:1, pitch:53 ,status:"note_off"})
+    ),
+
+    "/sound/tambourine/bang":(
+        ("MIDI", {channel:1, pitch:54 ,status:"note_on"})
+    ),
+    "/sound/tambourine/off":(
+        ("MIDI", {channel:1, pitch:54 ,status:"note_off"})
+    ),
+
+    "/sound/splash_cymbal/bang":(
+        ("MIDI", {channel:1, pitch:55 ,status:"note_on"})
+    ),
+    "/sound/splash_cymbal/off":(
+        ("MIDI", {channel:1, pitch:55 ,status:"note_off"})
+    ),
+
+    "/sound/cowbell/bang":(
+        ("MIDI", {channel:1, pitch:56 ,status:"note_on"})
+    ),
+    "/sound/cowbell/off":(
+        ("MIDI", {channel:1, pitch:56 ,status:"note_off"})
+    ),
+
+    "/sound/crash_cymbal2/bang":(
+        ("MIDI", {channel:1, pitch:57 ,status:"note_on"})
+    ),
+    "/sound/crash_cymbal2/off":(
+        ("MIDI", {channel:1, pitch:57 ,status:"note_off"})
+    ),
+
+    "/sound/vibra_slap/bang":(
+        ("MIDI", {channel:1, pitch:58 ,status:"note_on"})
+    ),
+    "/sound/vibra_slap/off":(
+        ("MIDI", {channel:1, pitch:58 ,status:"note_off"})
+    ),
+
+    "/sound/ride_cymbal2/bang":(
+        ("MIDI", {channel:1, pitch:59 ,status:"note_on"})
+    ),
+    "/sound/ride_cymbal2/off":(
+        ("MIDI", {channel:1, pitch:59 ,status:"note_off"})
+    ),
+
+    "/sound/high_bongo/bang":(
+        ("MIDI", {channel:1, pitch:60 ,status:"note_on"})
+    ),
+    "/sound/high_bongo/off":(
+        ("MIDI", {channel:1, pitch:60 ,status:"note_off"})
+    ),
+
+    "/sound/low_bongo/bang":(
+        ("MIDI", {channel:1, pitch:61 ,status:"note_on"})
+    ),
+    "/sound/low_bongo/off":(
+        ("MIDI", {channel:1, pitch:61 ,status:"note_off"})
+    ),
+
+    "/sound/mute_high_conga/bang":(
+        ("MIDI", {channel:1, pitch:62 ,status:"note_on"})
+    ),
+    "/sound/mute_high_conga/off":(
+        ("MIDI", {channel:1, pitch:62 ,status:"note_off"})
+    ),
+
+    "/sound/open_high_conga/bang":(
+        ("MIDI", {channel:1, pitch:63 ,status:"note_on"})
+    ),
+    "/sound/open_high_conga/off":(
+        ("MIDI", {channel:1, pitch:63 ,status:"note_off"})
+    ),
+
+    "/sound/low_conga/bang":(
+        ("MIDI", {channel:1, pitch:64 ,status:"note_on"})
+    ),
+    "/sound/low_conga/off":(
+        ("MIDI", {channel:1, pitch:64 ,status:"note_off"})
+    ),
+
+    "/sound/high_timbale/bang":(
+        ("MIDI", {channel:1, pitch:65 ,status:"note_on"})
+    ),
+    "/sound/high_timbale/off":(
+        ("MIDI", {channel:1, pitch:65 ,status:"note_off"})
+    ),
+
+    "/sound/low_timbale/bang":(
+        ("MIDI", {channel:1, pitch:66 ,status:"note_on"})
+    ),
+    "/sound/low_timbale/off":(
+        ("MIDI", {channel:1, pitch:66 ,status:"note_off"})
+    ),
+
+    "/sound/high_agogo/bang":(
         ("MIDI", {channel:1, pitch:67 ,status:"note_on"})
     ),
-    "/sound/perc_3/off":(
+    "/sound/high_agogo/off":(
         ("MIDI", {channel:1, pitch:67 ,status:"note_off"})
     ),
 
-    "/sound/perc_4/bang":(
+    "/sound/low_agogo/bang":(
         ("MIDI", {channel:1, pitch:68 ,status:"note_on"})
     ),
-    "/sound/perc_4/off":(
+    "/sound/low_agogo/off":(
         ("MIDI", {channel:1, pitch:68 ,status:"note_off"})
     ),
+
+    "/sound/cabasa/bang":(
+        ("MIDI", {channel:1, pitch:69 ,status:"note_on"})
+    ),
+    "/sound/cabasa/off":(
+        ("MIDI", {channel:1, pitch:69 ,status:"note_off"})
+    ),
+
+    "/sound/maracas/bang":(
+        ("MIDI", {channel:1, pitch:70 ,status:"note_on"})
+    ),
+    "/sound/maracas/off":(
+        ("MIDI", {channel:1, pitch:70 ,status:"note_off"})
+    ),
+
+    "/sound/short_whistle/bang":(
+        ("MIDI", {channel:1, pitch:71 ,status:"note_on"})
+    ),
+    "/sound/short_whistle/off":(
+        ("MIDI", {channel:1, pitch:71 ,status:"note_off"})
+    ),
+
+    "/sound/long_whistle/bang":(
+        ("MIDI", {channel:1, pitch:72 ,status:"note_on"})
+    ),
+    "/sound/long_whistle/off":(
+        ("MIDI", {channel:1, pitch:72 ,status:"note_off"})
+    ),
+
+    "/sound/short_guiro/bang":(
+        ("MIDI", {channel:1, pitch:73 ,status:"note_on"})
+    ),
+    "/sound/short_guiro/off":(
+        ("MIDI", {channel:1, pitch:73 ,status:"note_off"})
+    ),
+
+    "/sound/long_guiro/bang":(
+        ("MIDI", {channel:1, pitch:74 ,status:"note_on"})
+    ),
+    "/sound/long_guiro/off":(
+        ("MIDI", {channel:1, pitch:74 ,status:"note_off"})
+    ),
+
+    "/sound/claves/bang":(
+        ("MIDI", {channel:1, pitch:75 ,status:"note_on"})
+    ),
+    "/sound/claves/off":(
+        ("MIDI", {channel:1, pitch:75 ,status:"note_off"})
+    ),
+
+    "/sound/high_woodblock/bang":(
+        ("MIDI", {channel:1, pitch:76 ,status:"note_on"})
+    ),
+    "/sound/high_woodblock/off":(
+        ("MIDI", {channel:1, pitch:76 ,status:"note_off"})
+    ),
+
+    "/sound/low_woodblock/bang":(
+        ("MIDI", {channel:1, pitch:77 ,status:"note_on"})
+    ),
+    "/sound/low_woodblock/off":(
+        ("MIDI", {channel:1, pitch:77 ,status:"note_off"})
+    ),
+
+    "/sound/mute_cuica/bang":(
+        ("MIDI", {channel:1, pitch:78 ,status:"note_on"})
+    ),
+    "/sound/mute_cuica/off":(
+        ("MIDI", {channel:1, pitch:78 ,status:"note_off"})
+    ),
+
+    "/sound/open_cuica/bang":(
+        ("MIDI", {channel:1, pitch:79 ,status:"note_on"})
+    ),
+    "/sound/open_cuica/off":(
+        ("MIDI", {channel:1, pitch:79 ,status:"note_off"})
+    ),
+
+    "/sound/mute_triangle/bang":(
+        ("MIDI", {channel:1, pitch:80 ,status:"note_on"})
+    ),
+    "/sound/mute_triangle/off":(
+        ("MIDI", {channel:1, pitch:80 ,status:"note_off"})
+    ),
+
+    "/sound/open_triangle/bang":(
+        ("MIDI", {channel:1, pitch:81 ,status:"note_on"})
+    ),
+    "/sound/open_triangle/off":(
+        ("MIDI", {channel:1, pitch:81 ,status:"note_off"})
+    )
 }
