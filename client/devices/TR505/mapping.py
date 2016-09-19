60804c78554e97aee4460a75860e8cf6c39814f8
João Costa <joaopedrocosta@me.com>
2016-09-19 15:34:35 -0400

    added RY10 and TR505

--- client/devices/TR505/mapping.py
+++ client/devices/TR505/mapping.py
@@ -1,196 +1,120 @@
 instruments = (
     "_system",
-    "_general",
-    # "_kick",
-    # "_snare",
-    # "_tom",
-    # "_hihat",
-    # "_cymbal",
-    # "_percussion",
-    # "_miscellaneous"
+    "_general"
 )
 
 
 mapping = {
-    "/sound/tom_1/bang":(
-        ("MIDI", {"channel":1, "pitch":48 ,"status":"note_on"})
-    ),
-    "/sound/tom_1/off":(
-        ("MIDI", {"channel":1, "pitch":48 ,"status":"note_off"})
-    ),
-
-    "/sound/tom_2/bang":(
-        ("MIDI", {"channel":1, "pitch":48 ,"status":"note_on"})
-    ),
-    "/sound/tom_2/off":(
-        ("MIDI", {"channel":1, "pitch":48 ,"status":"note_off"})
-    ),
-
-    "/sound//bang":(
-        ("MIDI", {"channel":1, "pitch":45 ,"status":"note_on"})
-    ),
-    "/sound//off":(
-        ("MIDI", {"channel":1, "pitch":45 ,"status":"note_off"})
-    ),
-
-    "/sound/tom_3/bang":(
-        ("MIDI", {"channel":1, "pitch":41 ,"status":"note_on"})
-    ),
-    "/sound/tom_3/off":(
-        ("MIDI", {"channel":1, "pitch":41 ,"status":"note_off"})
-    ),
-
-    "/sound/tom_4/bang":(
-        ("MIDI", {"channel":1, "pitch":63 ,"status":"note_on"})
-    ),
-    "/sound/tom_4/off":(
-        ("MIDI", {"channel":1, "pitch":63 ,"status":"note_off"})
-    ),
-
-    "/sound/ride/bang":(
-        ("MIDI", {"channel":1, "pitch":51 ,"status":"note_on"})
-    ),
-    "/sound/ride/off":(
-        ("MIDI", {"channel":1, "pitch":51 ,"status":"note_off"})
-    ),
-
-    "/sound/crash/bang":(
-        ("MIDI", {"channel":1, "pitch":49 ,"status":"note_on"})
-    ),
-    "/sound/crash/off":(
-        ("MIDI", {"channel":1, "pitch":49 ,"status":"note_off"})
-    ),
-
-    "/sound/perc_1/bang":(
-        ("MIDI", {"channel":1, "pitch":65 ,"status":"note_on"})
-    ),
-    "/sound/perc_1/off":(
-        ("MIDI", {"channel":1, "pitch":65 ,"status":"note_off"})
-    ),
-
-    "/sound/perc_2/bang":(
-        ("MIDI", {"channel":1, "pitch":62 ,"status":"note_on"})
-    ),
-    "/sound/perc_2/off":(
-        ("MIDI", {"channel":1, "pitch":62 ,"status":"note_off"})
-    ),
-
-    "/sound/kick/bang":(
+    "_general/sound/bass_drum/bang":(
         ("MIDI", {"channel":1, "pitch":35 ,"status":"note_on"})
     ),
-    "/sound/kick/off":(
+    "_general/sound/bass_drum/off":(
         ("MIDI", {"channel":1, "pitch":35 ,"status":"note_off"})
     ),
 
-    "/sound/snare/bang":(
+    "_general/sound/side_stick/bang":(
+        ("MIDI", {"channel":1, "pitch":37 ,"status":"note_on"})
+    ),
+    "_general/sound/side_stick/off":(
+        ("MIDI", {"channel":1, "pitch":37 ,"status":"note_off"})
+    ),
+
+    "_general/sound/snare_drum1/bang":(
         ("MIDI", {"channel":1, "pitch":38 ,"status":"note_on"})
     ),
-    "/sound/snare/off":(
+    "_general/sound/snare_drum1/off":(
         ("MIDI", {"channel":1, "pitch":38 ,"status":"note_off"})
     ),
 
-    "/sound/closed_hat/bang":(
-        ("MIDI", {"channel":1, "pitch":42 ,"status":"note_on"})
-    ),
-    "/sound/closed_hat/off":(
-        ("MIDI", {"channel":1, "pitch":42 ,"status":"note_off"})
-    ),
-
-    "/sound/mid_hat/bang":(
-        ("MIDI", {"channel":1, "pitch":44 ,"status":"note_on"})
-    ),
-    "/sound/mid_hat/off":(
-        ("MIDI", {"channel":1, "pitch":44 ,"status":"note_off"})
-    ),
-
-    "/sound/open_hat/bang":(
-        ("MIDI", {"channel":1, "pitch":46 ,"status":"note_on"})
-    ),
-    "/sound/open_hat/off":(
-        ("MIDI", {"channel":1, "pitch":46 ,"status":"note_off"})
-    ),
-
-    "/sound/claps/bang":(
+    "_general/sound/hand_clap/bang":(
         ("MIDI", {"channel":1, "pitch":39 ,"status":"note_on"})
     ),
-    "/sound/clapsperc_3/off":(
+    "_general/sound/hand_clap/off":(
         ("MIDI", {"channel":1, "pitch":39 ,"status":"note_off"})
     ),
 
-    "/sound/perc_3/bang":(
+    "_general/sound/low_tom1/bang":(
+        ("MIDI", {"channel":1, "pitch":41 ,"status":"note_on"})
+    ),
+    "_general/sound/low_tom1/off":(
+        ("MIDI", {"channel":1, "pitch":41 ,"status":"note_off"})
+    ),
+
+    "_general/sound/closed_hihat/bang":(
+        ("MIDI", {"channel":1, "pitch":42 ,"status":"note_on"})
+    ),
+    "_general/sound/closed_hihat/off":(
+        ("MIDI", {"channel":1, "pitch":42 ,"status":"note_off"})
+    ),
+
+    "_general/sound/mid_tom1/bang":(
+        ("MIDI", {"channel":1, "pitch":45 ,"status":"note_on"})
+    ),
+    "_general/sound/mid_tom1/off":(
+        ("MIDI", {"channel":1, "pitch":45 ,"status":"note_off"})
+    ),
+
+    "_general/sound/open_hihat/bang":(
+        ("MIDI", {"channel":1, "pitch":46 ,"status":"note_on"})
+    ),
+    "_general/sound/open_hihat/off":(
+        ("MIDI", {"channel":1, "pitch":46 ,"status":"note_off"})
+    ),
+
+    "_general/sound/high_tom1/bang":(
+        ("MIDI", {"channel":1, "pitch":48 ,"status":"note_on"})
+    ),
+    "_general/sound/high_tom1/off":(
+        ("MIDI", {"channel":1, "pitch":48 ,"status":"note_off"})
+    ),
+
+    "_general/sound/crash_cymbal/bang":(
+        ("MIDI", {"channel":1, "pitch":49 ,"status":"note_on"})
+    ),
+    "_general/sound/crash_cymbal/off":(
+        ("MIDI", {"channel":1, "pitch":49 ,"status":"note_off"})
+    ),
+
+    "_general/sound/ride_cymbal1/bang":(
+        ("MIDI", {"channel":1, "pitch":51 ,"status":"note_on"})
+    ),
+    "_general/sound/ride_cymbal1/off":(
+        ("MIDI", {"channel":1, "pitch":51 ,"status":"note_off"})
+    ),
+
+    "_general/sound/mute_high_conga/bang":(
+        ("MIDI", {"channel":1, "pitch":62 ,"status":"note_on"})
+    ),
+    "_general/sound/mute_high_conga/off":(
+        ("MIDI", {"channel":1, "pitch":62 ,"status":"note_off"})
+    ),
+
+    "_general/sound/low_conga/bang":(
+        ("MIDI", {"channel":1, "pitch":63 ,"status":"note_on"})
+    ),
+    "_general/sound/low_conga/off":(
+        ("MIDI", {"channel":1, "pitch":63 ,"status":"note_off"})
+    ),
+
+    "_general/sound/high_timbale/bang":(
+        ("MIDI", {"channel":1, "pitch":65 ,"status":"note_on"})
+    ),
+    "_general/sound/high_timbale/off":(
+        ("MIDI", {"channel":1, "pitch":65 ,"status":"note_off"})
+    ),
+
+    "_general/sound/high_agogo/bang":(
         ("MIDI", {"channel":1, "pitch":67 ,"status":"note_on"})
     ),
-    "/sound/perc_3/off":(
+    "_general/sound/high_agogo/off":(
         ("MIDI", {"channel":1, "pitch":67 ,"status":"note_off"})
     ),
 
-    "/sound/perc_4/bang":(
+    "_general/sound/low_agogo/bang":(
         ("MIDI", {"channel":1, "pitch":68 ,"status":"note_on"})
     ),
-    "/sound/perc_4/off":(
+    "_general/sound/low_agogo/off":(
         ("MIDI", {"channel":1, "pitch":68 ,"status":"note_off"})
     ),
 
-    ##### KICK ######
-
-    "_kick/sound/kick_1/bang":(
-        ("MIDI", {"channel":1, "pitch":79, "status":"note_on"})
-    ),
-    "_kick/sound/kick_1/off":(
-        ("MIDI", {"channel":1, "pitch":79, "status":"note_off"})
-    ),
-
-    ##### SNARE ######
-
-    "_snare/sound/snare_1/bang":(
-        ("MIDI", {"channel":1, "pitch":79, "status":"note_on"})
-    ),
-    "_snare/sound/snare_1/off":(
-        ("MIDI", {"channel":1, "pitch":79, "status":"note_off"})
-    ),
-
-    ##### TOM ######
-
-    "_tom/sound/tom_1/bang":(
-        ("MIDI", {"channel":1, "pitch":79, "status":"note_on"})
-    ),
-    "_tom/sound/tom_1/off":(
-        ("MIDI", {"channel":1, "pitch":79, "status":"note_off"})
-    ),
-
-    ##### HIHAT ######
-
-    "_hihat/sound/hihat_1/bang":(
-        ("MIDI", {"channel":1, "pitch":79, "status":"note_on"})
-    ),
-    "_hihat/sound/hihat_1/off":(
-        ("MIDI", {"channel":1, "pitch":79, "status":"note_off"})
-    ),
-
-    ##### CYMBAL ######
-
-    "_cymbal/sound/cymbal_1/bang":(
-        ("MIDI", {"channel":1, "pitch":79, "status":"note_on"})
-    ),
-    "_cymbal/sound/cymbal_1/off":(
-        ("MIDI", {"channel":1, "pitch":79, "status":"note_off"})
-    ),
-
-    ##### PERCUSSION ######
-
-    "_percussion/sound/percussion_1/bang":(
-        ("MIDI", {"channel":1, "pitch":79, "status":"note_on"})
-    ),
-    "_percussion/sound/percussion_1/off":(
-        ("MIDI", {"channel":1, "pitch":79, "status":"note_off"})
-    ),
-
-    ##### MISCELLANEOUS ######
-
-    "_miscellaneous/sound/miscellaneous_1/bang":(
-        ("MIDI", {"channel":1, "pitch":79, "status":"note_on"})
-    ),
-    "_miscellaneous/sound/miscellaneous_1/off":(
-        ("MIDI", {"channel":1, "pitch":79, "status":"note_off"})
-    ),
 }
