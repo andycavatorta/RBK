86be404f9702fffd72140d4216fe2a845058014e
João Costa <joaopedrocosta@me.com>
2016-09-26 13:19:31 -0400

    fixing midioutput

--- common/midiOutput.py
+++ common/midiOutput.py
@@ -10,11 +10,13 @@ class Midi_Output():
             print self.midi_out
         except Exception as e:
             print 'trying 2x2..'
+            self.midi_out.close()
         try:
             self.midi_out = mido.open_output(oNames[1])
             print self.midi_out
         except Exception as e:
             print 'no MIDI interface connected!'
+            self.midi_out.close()
 
 
     def send_midi(self, params, status, channel, data1=None, data2=None):
