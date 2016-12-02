ecc7eaabf6b8cd3c60420788e8e39d6d10de3784
João Costa <joaopedrocosta@me.com>
2016-12-02 12:01:59 -0500

    got back to pre-fixed version

--- client/devices/AcetoneFR1/mapping.py
+++ client/devices/AcetoneFR1/mapping.py
@@ -7,93 +7,93 @@ instruments = (
 
 mapping = {
     "_general/sound/bass_drum/bang":(
-        ("pulse", {"channel":15, "function": "pulse", "pulselength":0.25 })
+        ("pulse", {"channel":16, "function": "pulse", "pulselength":0.25 })
     ),
     # "_general/sound/bass_drum/off":(
     #     ("digital", {"channel":21, "function": "digital", "bool":1 })
     # ),
 
     "_general/sound/snare_drum1/bang":(
-        ("pulse", {"channel":3, "function": "pulse", "pulselength":0.25 })
+        ("pulse", {"channel":4, "function": "pulse", "pulselength":0.25 })
     ),
     # "_general/sound/snare_drum1/off":(
     #     ("digital", {"channel":1, "value":38 ,"status":"note_off"})
     # ),
 
     "_general/sound/snare_drum2/bang":(
-        ("pulse", {"channel":6, "function": "pulse", "pulselength":0.25 })
+        ("pulse", {"channel":7, "function": "pulse", "pulselength":0.25 })
     ),
     # "_general/sound/snare_drum2/off":(
     #     ("digital", {"channel":1, "value":40 ,"status":"note_off"})
     # ),
 
     "_general/sound/crash_cymbal1/bang":(
-        ("pulse", {"channel":16, "function": "pulse", "pulselength":0.25 })
+        ("pulse", {"channel":17, "function": "pulse", "pulselength":0.25 })
     ),
     # "_general/sound/crash_cymbal1/off":(
     #     ("digital", {"channel":20, "function": "digital", "bool":1 })
     # ),
 
     "_general/sound/cowbell/bang":(
-        ("pulse", {"channel":10, "function": "pulse", "pulselength":0.25 })
+        ("pulse", {"channel":11, "function": "pulse", "pulselength":0.25 })
     ),
     # "_general/sound/cowbell/off":(
     #     ("digital", {"channel":18, "function": "digital", "bool":1 })
     # ),
 
     "_general/sound/high_bongo/bang":(
-        ("pulse", {"channel":9, "function": "pulse", "pulselength":0.25 })
+        ("pulse", {"channel":10, "function": "pulse", "pulselength":0.25 })
     ),
     # "_general/sound/high_bongo/off":(
     #     ("digital", {"channel":1, "value":60 ,"status":"note_off"})
     # ),
 
     "_general/sound/low_bongo/bang":(
-        ("pulse", {"channel":8, "function": "pulse", "pulselength":0.25 })
+        ("pulse", {"channel":9, "function": "pulse", "pulselength":0.25 })
     ),
     # "_general/sound/low_bongo/off":(
     #     ("digital", {"channel":1, "value":61 ,"status":"note_off"})
     # ),
 
     "_general/sound/low_conga/bang":(
-        ("pulse", {"channel":7, "function": "pulse", "pulselength":0.25 })
+        ("pulse", {"channel":8, "function": "pulse", "pulselength":0.25 })
     ),
     # "_general/sound/low_conga/off":(
     #     ("digital", {"channel":1, "value":64 ,"status":"note_off"})
     # ),
 
     "_general/sound/claves/bang":(
-        ("pulse", {"channel":11, "function": "pulse", "pulselength":0.25 })
+        ("pulse", {"channel":12, "function": "pulse", "pulselength":0.25 })
     ),
     # "_general/sound/claves/off":(
     #     ("digital", {"channel":19, "function": "digital", "bool":1 })
     # ),
 
     "_general/sound/maracas/bang":(
-        ("pulse", {"channel":12, "function": "pulse", "pulselength":0.25,})
+        ("pulse", {"channel":13, "function": "pulse", "pulselength":0.25,})
     ),
     # "_general/sound/maracas/off":(
     #     ("digital", {"channel":1, "value":70, "status":"note_off"})
     # ),
 
     "_general/control_change/Main_volume":(
-        ("square_wave", {"channel":2, "function": "square_wave", "frequency":30000})
+        ("square_wave", {"channel":3, "function": "square_wave", "frequency":30000})
     ),
 
     "_general/control_change/start":(
-        ("digital", {"channel":1, "function": "digital", "bool":1})
+        ("digital", {"channel":2, "function": "digital", "bool":1})
     ),
 
     "_general/control_change/stop":(
-        ("digital", {"channel":1, "function": "digital", "bool":0})
+        ("digital", {"channel":2, "function": "digital", "bool":0})
     ),
 
     "_general/control_change/int_ext":(
-        ("digital", {"channel":13, "function": "digital", "bool":1})
+        ("digital", {"channel":14, "function": "digital", "bool":1})
     ),
 
     "_general/control_change/clock":(
-        ("square_wave", {"channel":14, "function": "square_wave", "frequency":40})
+        ("square_wave", {"channel":15, "function": "square_wave", "frequency":40})
     ),
 
 
