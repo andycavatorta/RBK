ceee846a411d7fed5246fcac466bae871e60d4e1
João Costa <joaopedrocosta@me.com>
2016-12-09 15:51:47 -0500

    trying to fix low duty

--- common/signalOutputProcess.py
+++ common/signalOutputProcess.py
@@ -157,8 +157,10 @@ class Channel_Process(multiprocessing.Process):
                     x24bitParallelPort.send(list("%s%s%s" % (modeSelector_bin_str, channel_bin_str, osc_bin_str)))
                 if ds_b:
                     modeSelector_bin_str = "1"
-                    dutyCycle_bin_str = "000000000011" if self.dutyCycle > 99 else '{0:012b}'.format(int(self.dutyCycle))[::-1]
-                    padding_bin_str = "00000"
+                    dutyCycle_bin_str = "00000011" if self.dutyCycle > 99 else '{0:08b}'.format(int(self.dutyCycle))[::-1]
+                    padding_bin_str = "000000000"
+                    print self.dutyCycle
+                    print dutyCycle_bin_str
                     print ("%s%s%s%s" % (modeSelector_bin_str, channel_bin_str, dutyCycle_bin_str, padding_bin_str))
                     x24bitParallelPort.send(list("%s%s%s%s" % (modeSelector_bin_str, channel_bin_str, dutyCycle_bin_str, padding_bin_str)))
 
