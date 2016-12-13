ea9949e7fdd0b61ff8f0424801307f80966b1ebe
João Costa <joaopedrocosta@me.com>
2016-12-13 15:16:01 -0500

    trying to fix duty cicle

--- common/signalOutputProcess.py
+++ common/signalOutputProcess.py
@@ -164,8 +164,8 @@ class Channel_Process(multiprocessing.Process):
                     print int((self.dutyCycle*0.64)+0.5)
                     # print int((self.dutyCycle*0.32)-1)
                     # padding_bin_str = "00000"
-                    print list("%s%s%s%s" % (modeSelector_bin_str, channel_bin_str, dutyCycle_bin_str))
-                    x24bitParallelPort.send(list("%s%s%s%s" % (modeSelector_bin_str, channel_bin_str, dutyCycle_bin_str)))
+                    print list("%s%s%s" % (modeSelector_bin_str, channel_bin_str, dutyCycle_bin_str))
+                    x24bitParallelPort.send(list("%s%s%s" % (modeSelector_bin_str, channel_bin_str, dutyCycle_bin_str)))
 
             def enqueue(self, params):
                 self.params = params
