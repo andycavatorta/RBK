056a38b21ea55c7d2c58385f523e26826e6b9c24
João Costa <joaopedrocosta@me.com>
2016-12-13 15:27:47 -0500

    trying to fix duty cicle

--- common/signalOutputProcess.py
+++ common/signalOutputProcess.py
@@ -154,12 +154,12 @@ class Channel_Process(multiprocessing.Process):
                 channel_bin_str = '{:05b}'.format(self.channel)
                 if freq_b:
                     modeSelector_bin_str = "0"
-                    osc_bin_str = ('{0:017b}'.format(int(FPGA_CLOCK_SPEED_DIVIDED / self.freq)))[::-1] 
+                    osc_bin_str = ('{:017b}'.format(int(FPGA_CLOCK_SPEED_DIVIDED / self.freq)))
                     x24bitParallelPort.send(list("%s%s%s" % (modeSelector_bin_str, channel_bin_str, osc_bin_str)))
                 if ds_b:
                     modeSelector_bin_str = "1"
                     # dutyCycle_bin_str = "000011" if self.dutyCycle > 99 else '{0:06b}'.format(int(max(0,int(self.dutyCycle*0.32)-1)))[::-1]
-                    dutyCycle_bin_str = "000011" if self.dutyCycle > 99 else '{0:06b}'.format(int((self.dutyCycle*0.64)+0.5))[::-1]
+                    dutyCycle_bin_str = "000011" if self.dutyCycle > 99 else '{:06b}'.format(int((self.dutyCycle*0.64)+0.5))
                     print dutyCycle_bin_str
                     print channel_bin_str
                     print int((self.dutyCycle*0.64)+0.5)
