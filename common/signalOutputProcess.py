781c34f113f507a827fa86485cc9d5c614bcb129
João Costa <joaopedrocosta@me.com>
2016-12-14 12:04:28 -0500

    trying to fix duty cycle

--- common/signalOutputProcess.py
+++ common/signalOutputProcess.py
@@ -162,7 +162,7 @@ class Channel_Process(multiprocessing.Process):
                     dutyCycle_bin_str = "000011" if self.dutyCycle > 99 else '{0:07b}'.format(int((self.dutyCycle*0.64)+0.5))[::-1]
                     print dutyCycle_bin_str
                     print channel_bin_str
-                    print int((self.dutyCycle*0.32)+0.5)
+                    print int((self.dutyCycle*0.64)+0.5)
                     # print int((self.dutyCycle*0.32)-1)
                     padding_bin_str = "00000000000"
                     print list("%s%s%s%s" % (modeSelector_bin_str, channel_bin_str, dutyCycle_bin_str, padding_bin_str))
