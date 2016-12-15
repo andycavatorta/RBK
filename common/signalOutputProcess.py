ad67c894b442126d1c59387ed9beffc6407cf471
Joao Costa <joaopedrocosta@me.com>
2016-12-15 14:28:26 -0500

    trying to fix low frequencies

--- common/signalOutputProcess.py
+++ common/signalOutputProcess.py
@@ -116,13 +116,13 @@ class Channel_Process(multiprocessing.Process):
                             if self.lowFreqSineToggle:
                                 if self.lowFreqSineDutyCycle > 0:
                                     self.digital(True)
-                                print "1: " , self.lowFreqSinePeriod * (self.lowFreqSineDutyCycle/50.0)
+                                # print "1: " , self.lowFreqSinePeriod * (self.lowFreqSineDutyCycle/50.0)
                                 time.sleep(self.lowFreqSinePeriod * (self.lowFreqSineDutyCycle/50.0))
                                 # self.lowFreqSineActive = False
                             else:
                                 if self.lowFreqSineDutyCycle < 100:
                                     self.digital(False)
-                                print "2: " ,self.lowFreqSinePeriod * ( (self.lowFreqSineDutyCycle/50.0) ) 
+                                # print "2: " ,self.lowFreqSinePeriod * ( (self.lowFreqSineDutyCycle/50.0) ) 
                                 time.sleep(self.lowFreqSinePeriod * (  (self.lowFreqSineDutyCycle/50.0) ) )
                                 # self.lowFreqSineActive = False
                             self.lowFreqSineToggle = not self.lowFreqSineToggle
