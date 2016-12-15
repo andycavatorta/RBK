4f4fb5cc401ed951c20219b0c93689ef101cd323
Joao Costa <joaopedrocosta@me.com>
2016-12-15 17:16:08 -0500

    trying to fix low frequencies

--- common/signalOutputProcess.py
+++ common/signalOutputProcess.py
@@ -156,7 +156,7 @@ class Channel_Process(multiprocessing.Process):
                     modeSelector_bin_str = "0"
                     osc_bin_str = ('{0:017b}'.format(int(FPGA_CLOCK_SPEED_DIVIDED / self.freq)))[::-1]
                     print int(FPGA_CLOCK_SPEED_DIVIDED/self.freq)
-                    x24bitParallelPort.send(list("%s%s%s0" % (modeSelector_bin_str, channel_bin_str, osc_bin_str)))
+                    x24bitParallelPort.send(list("%s%s%s" % (modeSelector_bin_str, channel_bin_str, osc_bin_str)))
                     print list("%s%s%s0" % (modeSelector_bin_str, channel_bin_str, osc_bin_str))
                 if ds_b:
                     modeSelector_bin_str = "1"
@@ -166,7 +166,7 @@ class Channel_Process(multiprocessing.Process):
                     # print channel_bin_str
                     # print int((self.dutyCycle*0.64)+0.5)
                     # print int((self.dutyCycle*0.32)-1)
-                    padding_bin_str = "000000000000"
+                    padding_bin_str = "00000"
                     # print list("%s%s%s%s" % (modeSelector_bin_str, channel_bin_str, dutyCycle_bin_str, padding_bin_str))
                     x24bitParallelPort.send(list("%s%s%s%s" % (modeSelector_bin_str, channel_bin_str, dutyCycle_bin_str, padding_bin_str)))
 
