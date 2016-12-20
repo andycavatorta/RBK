d9ad2530eb5baa64dba6931fe342b787fda1ea3a
João Costa <joaopedrocosta@me.com>
2016-12-20 12:15:16 -0500

    new version

--- common/upgradeScripts.py
+++ common/upgradeScripts.py
@@ -7,4 +7,7 @@ scripts = {
     "0.02":[
         "echo running upgrade scripts for version 0.02"
     ],
+    "0.03":[
+        "sudo python /home/pi/RBK/common/configuremojo.py -v -n -d=/dev/ttyACM0 -i=/home/pi/RBK/fpga/mojo-base-project-master-sig-gen/syn/mojo_top.bin"
+    ],
 }
