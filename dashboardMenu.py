ed907abc9ba15755fc6661ed1e7b573990199ea2
João Costa <joaopedrocosta@me.com>
2016-12-26 14:43:05 -0500

    adding menu

--- dashboardMenu.py
+++ dashboardMenu.py
@@ -6,7 +6,7 @@ import signalOutputProcess as sop
 
 class Dashboard(threading.Thread):
   def __init__(self):
-      threading.Thread.__init__(self) 
+    threading.Thread.__init__(self) 
 
   def run(self):
     pass
