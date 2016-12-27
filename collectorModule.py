e7e1e4a0ce968afb42ff89efb43397acb9f36170
João Costa <joaopedrocosta@me.com>
2016-12-27 13:44:24 -0500

    adding menu

--- collectorModule.py
+++ collectorModule.py
@@ -6,6 +6,8 @@ class Collector(threading.Thread):
     threading.Thread.__init__(self)
     self.messages = []
     self.final_list = []
+    self.osc_messages = []
+    self.final_osc = []
 
   def run(self):
     pass
@@ -13,7 +15,12 @@ class Collector(threading.Thread):
   def collect(self,_type, _msg):
     self.msg = _msg
     self.type = _type
-    self.messages.append((self.type,self.msg)) 
+    self.messages.append((self.type,self.msg))
+
+  def collect_osc(self, _type, _msg):
+    self.msg = _msg
+    self.type = _type
+    self.osc_messages.append((self.type, self.msg))
 
   def get(self, _filter):
     self.filter = _filter
@@ -25,6 +32,18 @@ class Collector(threading.Thread):
       for element in self.final_list:
         print element
     del self.final_list[:]
+    del self.messages[:]
+
+    def get_osc(self, _filter):
+    self.filter = _filter
+    if self.filter == "all":
+      for element in self.osc_messages:
+        print element
+    else:
+      self.final_osc = [self.t for self.t in self.messages if self.t[0].startswith(self.filter)]
+      for element in self.final_osc:
+        print element
+    del self.final_osc[:]
     del self.messages[:]
 
 
