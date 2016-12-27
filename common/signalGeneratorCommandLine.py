f330ffb01b8bfb8fbe80043532cdc446185293cb
João Costa <joaopedrocosta@me.com>
2016-12-27 12:18:27 -0500

    adding collector

--- common/signalGeneratorCommandLine.py
+++ common/signalGeneratorCommandLine.py
@@ -47,7 +47,7 @@ class CommandLine_Funcs(multiprocessing.Process):
         if 0<= self.input_int <=23:
           self.params["channel"] = self.input_int
           self.menuFunction(self.params)
-        else:
+        if self.input_raw == "?":
           break
       except Exception as e:
         print "menuChannel:please enter a number between 0 and 23:"
