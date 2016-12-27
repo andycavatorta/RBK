b1221c1eee6559ababbb1cd0786cb23641ca7842
João Costa <joaopedrocosta@me.com>
2016-12-27 14:04:45 -0500

    adding live mode

--- dashboardMenu.py
+++ dashboardMenu.py
@@ -18,7 +18,8 @@ class Dashboard(threading.Thread):
     self.fmap = {
       "s":self.signal_generator,
       "p":self.performance_mode,
-      "o":self.raw_osc
+      "o":self.raw_osc,
+      "l":self.live_mode
     }
     self.goodValue = False
     while True: 
@@ -29,6 +30,7 @@ class Dashboard(threading.Thread):
       self.display("  s - Signal Generator\n")
       self.display("  p - Performance Mode\n")
       self.display("  o - Raw Osc Msgs\n")
+      self.display("  l - Live Mode\n")
       self.input = sys.stdin.readline()
       try:
         self.fmap[self.input[:-1]]()
@@ -72,5 +74,20 @@ class Dashboard(threading.Thread):
       except Exception as e:
         print "raw_osc:invalid value:", input
     return 
+
+  def live_mode(self):
+    self.goodValue = False
+    while self.goodValue == False:
+      self.display("Filter by: network, MIDI, pulse, square_wave, digital, all\n")
+      self.display("Type '?' and hit Enter anytime to exit:\n")
+      self.input_raw = sys.stdin.readline()
+      try:
+        self.input_f = self.input_raw[:-1]
+        self.collector.get(self.input_f)
+        if self.input_f == '?'
+          self.goodValue = True
+      except Exception as e:
+        print "live_mode:invalid value:", input
+    return
 
 
