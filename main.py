ac77b502980105026a6abdabe4324e5afd9c455e
João Costa <joaopedrocosta@me.com>
2017-01-31 14:05:56 -0500

    updating acetone mapping

--- main.py
+++ main.py
@@ -154,10 +154,10 @@ try:
                         for signal in iterate_device_mapping:
                             print repr(signal)
                             if signal['function'] == "square_wave":
-                                if signal['duty_cycle'] is None:
+                                if signal['variable_key'] is "duty_cycle":
                                     print "there"
                                     signal['duty_cycle'] = ((msg['params']['value']*(signal['duty_min_max'][1]-signal['duty_min_max'][0])/127)+signal['duty_min_max'][0])
-                                elif signal['frequency'] is None:
+                                elif signal['variable_key'] is "frequency":
                                     print "here"
                                     signal['frequency'] = ((msg['params']['value']*(signal['freq_min_max'][1]-signal['freq_min_max'][0])/127)+signal['freq_min_max'][0]) 
                             elif signal['function'] == "digital":
