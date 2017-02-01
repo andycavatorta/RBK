b69e9ac6a7e29b8b7928a435ba56d2ecc19e55a4
João Costa <joaopedrocosta@me.com>
2017-02-01 13:22:13 -0500

    updating git output

--- common/signalOutput.py
+++ common/signalOutput.py
@@ -119,7 +119,7 @@ class Channel(threading.Thread):
 
     def pulse(self,pulselength):
         # print 'Pulse | length: ', pulselength
-        # self.sendStateToFPGA(1,0)
+        self.sendStateToFPGA(1,0)
         self.dutyCycle = 100.0
         self.sendStateToFPGA(0,1)
         time.sleep(pulselength)
@@ -128,7 +128,7 @@ class Channel(threading.Thread):
 
     def digital(self,bool):
         # print "Digital | Bool: ", bool
-        # self.sendStateToFPGA(1,0)
+        self.sendStateToFPGA(1,0)
         self.dutyCycle = 100.0 if bool else 0.0
         self.sendStateToFPGA(0,1)
 
