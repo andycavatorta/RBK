7755d14c253682aed63a4bd6b149f56dbac1bd33
João Costa <joaopedrocosta@me.com>
2017-02-02 13:03:11 -0500

    fixing sq wave digital

--- common/signalOutput.py
+++ common/signalOutput.py
@@ -107,11 +107,13 @@ class Channel(threading.Thread):
                         if self.lowFreqSineDutyCycle > 0:
                             self.digital(True)
                         time.sleep(self.lowFreqSinePeriod * (self.lowFreqSineDutyCycle/100.0))
+                        print self.lowFreqSinePeriod * (self.lowFreqSineDutyCycle/100.0)
                         # self.lowFreqSineActive = False
                     else:
                         if self.lowFreqSineDutyCycle < 100:
                             self.digital(False)
                         time.sleep(self.lowFreqSinePeriod * ( 1 - (self.lowFreqSineDutyCycle/100.0) ) )
+                        print self.lowFreqSinePeriod * ( 1 - (self.lowFreqSineDutyCycle/100.0) )
                         # self.lowFreqSineActive = False
                     self.lowFreqSineToggle = not self.lowFreqSineToggle
                 else:
@@ -147,14 +149,14 @@ class Channel(threading.Thread):
         if freq_b:
             modeSelector_bin_str = "0"
             osc_bin_str = ('{0:017b}'.format(int(FPGA_CLOCK_SPEED_DIVIDED / self.freq)))[::-1]
-            print "channel: %s | freq: %s " % (self.channel, self.freq)
+            # print "channel: %s | freq: %s " % (self.channel, self.freq)
             x24bitParallelPort.send(list("%s%s%s0" % (modeSelector_bin_str, channel_bin_str, osc_bin_str)))
         if ds_b:
             modeSelector_bin_str = "1"
             # dutyCycle_bin_str = "000011" if self.dutyCycle > 99 else '{0:06b}'.format(int(max(0,int(self.dutyCycle*0.32)-1)))[::-1]
             dutyCycle_bin_str = "1111111" if self.dutyCycle > 99 else '{0:07b}'.format(int((self.dutyCycle*0.64)+0.5))[::-1]
             padding_bin_str = "00000000000"
-            print "channel: %s | duty: %s " % (self.channel, self.dutyCycle)
+            # print "channel: %s | duty: %s " % (self.channel, self.dutyCycle)
             x24bitParallelPort.send(list("%s%s%s%s" % (modeSelector_bin_str, channel_bin_str, dutyCycle_bin_str, padding_bin_str)))
 
     def enqueue(self, params):
