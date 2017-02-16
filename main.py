0ab545537352f1c93f7e0c7e7e0e5faf2a22c596
João Costa <joaopedrocosta@me.com>
2017-02-16 17:22:29 -0500

    updating mrq for mike

--- main.py
+++ main.py
@@ -174,17 +174,9 @@ try:
                             elif signal['function'] == "digital":
                                 if "bool" not in signal:
                                     if msg['params']['value'] < 64:
-                                        if "inverse" in signal:
-                                            print "HERE"
-                                            signal['bool'] = 1
-                                        else:
-                                            print "THERE"
-                                            signal['bool'] = 0
+                                        signal['bool'] = 0
                                     else:
-                                        if "inverse" in signal:
-                                            signal['bool'] = 0
-                                        else:
-                                            signal['bool'] = 1
+                                        signal['bool'] = 1
                             signal_output.send(signal)
                             collector.collect(device_mapping[0], "%s" % (signal))
             except Exception as e:
