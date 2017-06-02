85aaf0955a81bd14587c819899ca24de98e767fe
Andy Cavatorta <andycavatorta@gmail.com>
2017-06-02 23:13:16 +0200

    updating settings format

--- client/devices/MRQ/mapping.py
+++ client/devices/MRQ/mapping.py
@@ -9,17 +9,9 @@ mapping = {
     "_general/sound/bass_drum/bang":(
         ("signal", {"channel":4, "function": "pulse", "pulselength":0.02 })
     ),
-    # "_general/sound/bass_drum/off":(
-    #     ("digital", {"channel":21, "function": "digital", "bool":1 })
-    # ),
-
     "_general/sound/snare_drum1/bang":(
         ("signal", {"channel":5, "function": "pulse", "pulselength":0.02 })
     ),
-    # "_general/sound/snare_drum1/off":(
-    #     ("digital", {"channel":1, "value":38 ,"status":"note_off"})
-    # ),
-
     "_general/sound/snare_drum2/bang":(
         ("signal", {"channel":13, "function": "digital", "bool":1 }),
         ("signal", {"channel":23,"function": "digital", "bool":0})
@@ -41,55 +33,33 @@ mapping = {
     "_general/control_change/Main_Volume":(
         ("signal", {"channel":3, "function": "square_wave", "frequency":30000, "duty cycle":0, "freq_min_max":(30000,30000), "duty_min_max":(0,100), "variable_key":"duty_cycle"})
     ),
-
-    # "_general/control_change/start_stop":(
-    #     ("signal", {"channel":1, "function": "digital"}, {"channel":19, "function": "digital", "inverse":True})
-    # ),
-
     "_general/control_change/start":(
         ("signal", {"channel":1, "function": "digital", "bool":1}),
         ("signal", {"channel":19, "function": "digital", "bool":0})
     ),
-
     "_general/control_change/stop":(
         ("signal", {"channel":1, "function": "digital", "bool":0}),
         ("signal", {"channel":19, "function": "digital", "bool":1})
     ),
-
-
     "_general/control_change/int_ext":(
         ("signal", {"channel":0, "function": "digital"})
     ),
-
-    # "_general/control_change/clock":(
-    #     ("signal", {"channel":2, "function": "square_wave", "frequency":0, "duty cycle":50.0, "freq_min_max":(2,22), "duty_min_max":(0,100), "variable_key":"frequency"})
-    # ),
-
     "_general/control_change/master_tempo":(
         ("signal", {"channel":2, "function": "square_wave", "frequency":0, "duty cycle":50.0, "duty_min_max":(0,100), "variable_key":"frequency", "clock_factor":1.0})
     ),
-
-
-    ##### SYSTEM ######
-
-    # "_system/start":(
-    #     ("digital", {"channel":2, "function": "digital", "bool":1})
-    # ),
-
-    # "_system/start":(
-    #     ("digital", {"channel":2, "function": "digital", "bool":0})
-    # ),
-
-    # "_system/master_volume":(
-    #     ("square_wave", {"channel":3, "function": "square_wave", "frequency":30000, "duty":50.0})
-    # ),
-
-    # "_system/int_ext":(
-    #     ("digital", {"channel":14, "function": "digital", "bool":1})
-    # ),
-
-    # "_system/timing_clock":(
-    #     ("square_wave", {"channel":14, "function": "square_wave", "frequency":40, "duty":50.0})
-    # ),
-
+    "_system/start":(
+            ("digital", {"channel":2,"function": "digital", "bool":1})
+    ),
+    "_system/start":(
+        ("digital", {"channel":1, "function": "digital", "bool":0})
+    ),
+    "_system/master_volume":(
+        ("square_wave", {"channel":3, "function": "square_wave", "frequency":30000, "duty":50.0})
+    ),
+    "_system/int_ext":(
+        ("digital", {"channel":0, "function": "digital", "bool":1})
+    ),
+    "_system/timing_clock":(
+        ("square_wave", {"channel":2, "function": "square_wave", "frequency":40, "duty":50.0})
+    ),
 }
